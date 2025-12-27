629347c965067fafbb6e3141bb9036d4fd0bbbf2
Sandeep Shirole <sandeepshirole@rediffmail.com>
2025-12-27 19:35:24 +0530

    Day 2: Execution safety using __main__ and import-safe design

---
+++ 02_execution_and_imports/02_execution_and_imports/safe_import.py
@@ -0,0 +1,8 @@
+def run_process() -> None:
+    print("Connecting to database...")
+    print("Fetching user data...")
+    print("Process complete")
+
+
+if __name__ == "__main__":
+    run_process()
